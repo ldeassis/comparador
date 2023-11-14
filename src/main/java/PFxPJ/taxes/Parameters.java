08a95b1155eae178db24df0c575060d886d9d154
Leonard de Assis <lmassis@gmail.com>
2023-11-14 15:19:34 -0300

    fixing typos

--- src/main/java/PFxPJ/taxes/Parameters.java
+++ src/main/java/PFxPJ/taxes/Parameters.java
@@ -1,21 +1,6 @@
 package PFxPJ.taxes;
 
 public final class Parameters {
-    private Parameters() {
-    }
-
-    // Parametros do INSS
-
-    /**
-     * json default para a tabela do INSS
-     * Estará aqui até o json embutido funcionar corretamente
-     */
-    public static final String JSON_EMBEMBEDED_STRING = "{\"INSS\":[" +
-            "{\"min\": 0,\"max\": 1320.00,\"rate\": 0.075}," +
-            "{\"min\": 1320.01,\"max\": 2571.29,\"rate\": 0.09}," +
-            "{\"min\": 2571.30,\"max\": 3856.94,\"rate\": 0.12}," +
-            "{\"min\": 3856.95,\"max\": 7507.49,\"rate\": 0.14}]," +
-            "\"Teto INSS\": 876.95}";
     /**
      * String default para a localização da tabela embutida do INSS
      * Quando funcionar, o json String acima será apagado do código
